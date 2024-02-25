5adc5b5d872064b886b06c8aab3f393371b7a964
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-25 15:13:53 +0200

    Delete Quizz app directory

--- Quizz app/src/context/AppContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const AppContext = createContext({
-  user: null,
-  userData: null,
-  setContext() {
-    // real implementation comes from App.jsx
-  },
-});
