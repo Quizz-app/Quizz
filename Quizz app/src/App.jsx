4bdbde17cad9ef7cb9e627eec1482df0798737c9
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-25 14:58:29 +0200

    Fix git ignore problem

--- Quizz app/src/App.jsx
+++ Quizz app/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { AppContext } from "./context/AppContext";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Register from "./views/Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { auth } from "./config/firebase-config";
+import { getUserData } from "./services/users-service";
+import Home from "./views/Home";
+import Login from "./views/Login";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const [context, setContext] = useState({
+    user: null,
+    userData: null,
+  });
+
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      getUserData(user.uid)
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            setContext({ user, userData: snapshot.val()[Object.keys(snapshot.val())[0]] });
+          }
+        })
+    }
+  }, [user]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <AppContext.Provider value={{ ...context, setContext }}>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AppContext.Provider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
