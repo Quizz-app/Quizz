28d79f33d9469fb0e29291f18eca8592460a6e1a
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-25 13:47:11 +0200

    routing and simple home view

--- Quizz App/src/App.jsx
+++ Quizz App/src/App.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { AppContext } from "./context/AppContext";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Register from "./views/Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { auth } from "./config/firebase-config";
+import { getUserData } from "./services/users-service";
+import Home from "./views/Home";
+
 
 const App = () => {
-  return <Register />;
+
+  const [context, setContext] = useState({
+    user: null,
+    userData: null,
+  });
+
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      getUserData(user.uid)
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            setContext({ user, userData: snapshot.val()[Object.keys(snapshot.val())[0]] });
+          }
+        })
+    }
+  }, [user]);
+
+  return (
+    <BrowserRouter>
+      <AppContext.Provider value={{ ...context, setContext }}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AppContext.Provider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
