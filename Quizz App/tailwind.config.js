24b72ad544424be5af3daefcc24d0ad11d5d48e0
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-27 16:20:26 +0200

    Theme config

--- Quizz App/tailwind.config.js
+++ Quizz App/tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
 
 
-    
+
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -89,11 +89,41 @@ module.exports = {
           "secondary": "#f6d860",
           "accent": "#37cdbe",
           "neutral": "#3d4451",
-          "base-100": "#ffffff",
+          "base-100": "#FF7F50",
         },
       },
+      "light",
       "dark",
       "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
     ],
   },
 
