2a3cde85958e1c1b5ee85b70e2d4adf4eabcf1cd
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-02-27 15:36:24 +0200

    added daisyui

--- Quizz App/src/views/Home.jsx
+++ Quizz App/src/views/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <>
             <Button>Hello world</Button>
             <div className="mt-5 mb-5">
-                
+
 
             </div>
             <Calendar
@@ -20,6 +20,27 @@ const Home = () => {
                 onSelect={setDate}
                 className="rounded-md border shadow"
             />
+            <div className="stats shadow border">
+
+                <div className="stat place-items-center">
+                    <div className="stat-title">Downloads</div>
+                    <div className="stat-value">31K</div>
+                    <div className="stat-desc">From January 1st to February 1st</div>
+                </div>
+
+                <div className="stat place-items-center">
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value text-secondary">4,200</div>
+                    <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+                </div>
+
+                <div className="stat place-items-center">
+                    <div className="stat-title">New Registers</div>
+                    <div className="stat-value">1,200</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+            </div>
         </>
     );
 };
--- Quizz App/tailwind.config.js
+++ Quizz App/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "./node_modules/daisyui"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -73,5 +75,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [daisyui],
 }
