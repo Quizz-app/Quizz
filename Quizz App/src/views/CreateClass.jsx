3f2c1a40c5f0ba83cf5476e7793c5331bbe1d19f
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-03-13 22:42:19 +0200

    added some css to classes and teams

--- Quizz App/src/views/CreateClass.jsx
+++ Quizz App/src/views/CreateClass.jsx
@@ -4,6 +4,7 @@ import { getAllStudents } from "../services/users-service";
 import { onClassMembersChange, getClassById, removeMemberFromClass, inviteUserToClass, deleteClass, getAllClassQuizzes } from "../services/class-service.js";
 import TableRow from "../components/TableRow";
 import { AppContext } from "../context/AppContext";
+import { removeQuizFromTeam } from "../services/teams-service.js";
 
 const CreateClass = () => {
     const { userData, isLoading } = useContext(AppContext)
@@ -56,6 +57,10 @@ const CreateClass = () => {
         removeMemberFromClass(id, member);
     };
 
+    const handleRemoveQuiz = (quizId) => {
+        removeQuizFromTeam(id, quizId);
+    };
+
     const handleDeleteClass = () => {
         deleteClass(id);
         navigate('/my-classes');
@@ -64,63 +69,78 @@ const CreateClass = () => {
 
     const filteredStudents = students.filter(student => student.email.toLowerCase().includes(searchTerm.toLowerCase()));
     return (
-        <div>
-            <input className="input input-bordered w-24 md:w-auto mt-2 mb-2" type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} placeholder="Search for students" />
-            <div className="flex flex-col">
+        <div className="hero min-h-screen flex flex-col bg-base-200 rounded-lg">
+            <div className="hero-content text-center flex flex-col w-full">
+                <div>
+                    <input className="input input-bordered w-24 md:w-auto mt-2 mb-2" type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} placeholder="Search for students" />
+                </div>
+                <div className="w-full">
                 <div className="ml-10 mr-10">
-                    {searchTerm.length >= 3 && filteredStudents.map((student, index) =>
-                        <div className="border mb-2" key={index}>
-                            <label htmlFor="">Student username</label>
-                            <div className="border mb-3">
-                                {student.username}
+                        {searchTerm.length >= 3 &&
+                            <div className="overflow-x-auto">
+                                <h1 className="text-xl mb-5 mt-3">Results</h1>
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>Name</th>
+                                            <th>Job</th>
+                                            <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {filteredStudents.map((student, index) =>
+                                            <tr key={index}>
+                                                <th>{index + 1}</th>
+                                                <td>{student.username}</td>
+                                                <td>{student.role}</td>
+                                                <td>{student.email}</td>
+                                                <button onClick={() => handleInviteMember(student)} className="btn btn-xs">Add to team</button>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
                             </div>
-                            <button onClick={() => handleInviteMember(student)} className="ml-5">Add to class</button>
-                        </div>
-                        // <TableRow key={index} teacher={teacher} handleRemoveMember={() => handleRemoveMember(teacher)} />
-                    )}
-                </div>
-                {`Members of team ${classz.name}`}
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Username</th>
-                                <th>First and last name</th>
-                                <th>Email</th>
+                        }
+                    </div>
+                    <div className="divider"></div>
+                    <h1 className="text-2xl">Class members</h1>
+                    <div className="divider"></div>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>First and last name</th>
+                                    <th>Email</th>
 
-                            </tr>
-                        </thead>
-                        {members && userData && classz.creator && members.map((member, index) => (
-                            <TableRow key={index} teacher={member} creator={classz.creator.username} handleRemoveMember={() => handleRemoveMember(member)} />
-                        ))}
-                        <tfoot>
-                            <tr>
-                                <th>Username</th>
-                                <th>First and last name</th>
-                                <th>Email</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+                                </tr>
+                            </thead>
+                            {members && userData && classz.creator && members.map((member, index) => (
+                                <TableRow key={index} teacher={member} creator={classz.creator.username} handleRemoveMember={() => handleRemoveMember(member)} />
+                            ))}
+                        </table>
+                    </div>
                 </div>
-            </div>
-            <div>
-                {userData && classz.creator && (userData.isAdmin || userData.username === classz.creator.username) &&
-                    <button className="border" onClick={() => handleDeleteClass()}>Delete team</button>
-                }
-            </div>
-            <hr />
-            <div>
-                <h1>Class Quizzes</h1>
+                <div>
+                    {userData && classz.creator && (userData.isAdmin || userData.username === classz.creator.username) &&
+                        <button className="btn btn-primary" onClick={() => handleDeleteClass()}>Delete class</button>
+                    }
+                </div>
+                <hr />
+                <div>
+                    <h1>Class Quizzes</h1>
 
-                {classQuizzes.map((quiz, index) => (
-                    <div className="border flex flex-row justify-between" key={index}>
+                    {classQuizzes.map((quiz, index) => (
+                        <div className="border flex flex-row justify-between" key={index}>
                         <h2>{quiz.title}</h2>
                         <p>{quiz.description}</p>
                         <p>{quiz.creator}</p>
-                        <button onClick={() => navigate(`/quiz/${quiz.id}`)}>Go to quiz</button>
-                        <button>Remove quiz Trqbva da se opravi</button>
+                        <button onClick={() => navigate(`/quiz/${quiz.id}`)}>Got to quiz</button>
+                        <button onClick={() => handleRemoveQuiz(quiz.id)}>Remove quiz</button>
                     </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
