f30fe74d8b48f582ade96dad5aa2c9ced1baaf4c
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-03-16 19:09:23 +0200

    Try fix home page

--- Quizz App/tailwind.config.js
+++ Quizz App/tailwind.config.js
@@ -84,16 +84,78 @@ module.exports = {
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
+
+          moveHorizontal: {
+            "0%": {
+              transform: "translateX(-50%) translateY(-10%)",
+            },
+            "50%": {
+              transform: "translateX(50%) translateY(10%)",
+            },
+            "100%": {
+              transform: "translateX(-50%) translateY(-10%)",
+            },
+          },
+
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+
+
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+        
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        shimmer: "shimmer 2s linear infinite",
+
+        
+
       },
+      moveInCircle: {
+        "0%": {
+          transform: "rotate(0deg)",
+        },
+        "50%": {
+          transform: "rotate(180deg)",
+        },
+        "100%": {
+          transform: "rotate(360deg)",
+        },
+      },
+
+      moveVertical: {
+        "0%": {
+          transform: "translateY(-50%)",
+        },
+        "50%": {
+          transform: "translateY(50%)",
+        },
+        "100%": {
+          transform: "translateY(-50%)",
+        },
+      },
+
+      
+
+
+
       width: {
         '800px': '800px',
         '1000px': '1000px',
@@ -175,4 +237,5 @@ function addVariablesForColors({ addBase, theme }) {
     ":root": newVars,
   });
 }
+
 
