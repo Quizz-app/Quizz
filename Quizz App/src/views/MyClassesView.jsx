23c6b0ed1d056ef93a077fee186b18e58b481e60
Radoslav Kostadinov <bounslay@gmail.com>
2024-03-17 20:57:30 +0200

    Added component

--- Quizz App/src/views/MyClassesView.jsx
+++ Quizz App/src/views/MyClassesView.jsx
@@ -82,8 +82,6 @@ const MyClassesView = () => {
                         <Label htmlFor="description">Class description</Label>
                         <Input id="description" type="text" value={classData.description} onChange={updateForm("description")} />
                     </LabelInputContainer>
-                    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-3">
-                    </div>
                     <button
                         className=" mt-5 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 
                                             to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium 
@@ -94,20 +92,18 @@ const MyClassesView = () => {
                         Create class &rarr;
                         <BottomGradient />
                     </button>
-                    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
                 </DialogContent>
             </Dialog>
-
             {classes && classes.length > 0 && (
-                <div>
+                <div className="flex flex-row">
                     {classes.map((classItem, index) => (
                         classItem && classItem.name && classItem.description ? (
-                            <div key={index} className="card w-96 bg-base-100 shadow-xl image-full">
-                                <div className="justify-end card-body">
+                            <div key={index} className="card w-80 shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(144,238,144,0.9)] px-8 py-2 bg-[#90ee90] rounded-md text-dark font-light transition duration-200 ease-linear mr-6">
+                                <div className="card-body">
                                     <h2 className="card-title">{classItem.name}</h2>
                                     <p>{classItem.description}</p>
-                                    <div className="card-actions">
-                                        <Button variant="outline" onClick={() => navigate(`/class/${classItem.id}`)}>Go to class</Button>
+                                    <div className="flex justify-center items-center mt-5">
+                                        <button className="btn btn-sm bg-base-300 shadow-xl" onClick={() => navigate(`/class/${classItem.id}`)}>Go to class</button>
                                     </div>
                                 </div>
                             </div>
