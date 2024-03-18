fe781d08123c42b71b666129eaed9714b3bc0bf3
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2024-03-19 01:42:54 +0200

    if user is blocked you cant do any shit

--- Quizz App/src/App.jsx
+++ Quizz App/src/App.jsx
@@ -55,29 +55,35 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppContext.Provider value={{ ...context, setContext }}>
-        <Header theme={theme} onThemeChange={handleThemeChange} />
         <div className='min-h-screen min-w-screen flex flex-col ml-10 mr-10'>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/my-library" element={<AuthenticationRequired> <MyLibrary /> </AuthenticationRequired>} />
-            <Route path="/my-teams" element={<AuthenticationRequired> <MyTeamsView /> </AuthenticationRequired>} />
-            <Route path="/quiz-preview/:id" element={<AuthenticationRequired> <QuizPreview /> </AuthenticationRequired>} />
-            <Route path="/my-classes" element={<AuthenticationRequired> <MyClassesView /> </AuthenticationRequired>} />
-            <Route path="/quiz-solve/:id" element={<AuthenticationRequired> <QuizSolve /> </AuthenticationRequired>} />
-            <Route path="/team/:id" element={<AuthenticationRequired> <CreateTeam /> </AuthenticationRequired>} />
-            <Route path="/class/:id" element={<AuthenticationRequired> <CreateClass /> </AuthenticationRequired>} />
-            <Route path="/quiz/:id" element={<AuthenticationRequired> <CreateQuiz /> </AuthenticationRequired>} />
-            <Route path="/results/:id" element={<AuthenticationRequired> <QuizResults /> </AuthenticationRequired>} />
-            <Route path="/profile" element={<AuthenticationRequired> <Profile /> </AuthenticationRequired>} />
-            <Route path="/admin" element={<AuthenticationRequired><Admin /></AuthenticationRequired>} />
-            <Route path="/dashboard" element={<AuthenticationRequired> <Dashboard /> </AuthenticationRequired>} />
-          </Routes>
+          {context?.userData?.isBlocked ? (
+            <h1>You are blocked!</h1>
+          ) : (
+            <>
+              <Header theme={theme} onThemeChange={handleThemeChange} />
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route index element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/my-library" element={<AuthenticationRequired> <MyLibrary /> </AuthenticationRequired>} />
+                <Route path="/my-teams" element={<AuthenticationRequired> <MyTeamsView /> </AuthenticationRequired>} />
+                <Route path="/quiz-preview/:id" element={<AuthenticationRequired> <QuizPreview /> </AuthenticationRequired>} />
+                <Route path="/my-classes" element={<AuthenticationRequired> <MyClassesView /> </AuthenticationRequired>} />
+                <Route path="/quiz-solve/:id" element={<AuthenticationRequired> <QuizSolve /> </AuthenticationRequired>} />
+                <Route path="/team/:id" element={<AuthenticationRequired> <CreateTeam /> </AuthenticationRequired>} />
+                <Route path="/class/:id" element={<AuthenticationRequired> <CreateClass /> </AuthenticationRequired>} />
+                <Route path="/quiz/:id" element={<AuthenticationRequired> <CreateQuiz /> </AuthenticationRequired>} />
+                <Route path="/results/:id" element={<AuthenticationRequired> <QuizResults /> </AuthenticationRequired>} />
+                <Route path="/profile" element={<AuthenticationRequired> <Profile /> </AuthenticationRequired>} />
+                <Route path="/admin" element={<AuthenticationRequired><Admin /></AuthenticationRequired>} />
+                <Route path="/dashboard" element={<AuthenticationRequired> <Dashboard /> </AuthenticationRequired>} />
+              </Routes>
+            </>
+          )}
         </div>
         <Footer />
       </AppContext.Provider>
